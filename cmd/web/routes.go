17979d5ef69002970c5408008598cbda2f9d462c
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-30 00:16:43 -0400

    Initial commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/tahmid-saj/go-bookings-app/pkg/config"
+	"github.com/tahmid-saj/go-bookings-app/pkg/handlers"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
+
